0a1fb61853cd108faa2df75c81bc6132198e1a65
Marcel Stefko <marcel.stefko@epfl.ch>
2017-05-24 12:05:11 +0200

    Implemented PalmFluorophore

---
+++ src/algorithm_tester/generators/realtime/fluorophores/PalmFluorophore.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2017 stefko
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package algorithm_tester.generators.realtime.fluorophores;
+
+import algorithm_tester.generators.realtime.Camera;
+import algorithm_tester.generators.realtime.Fluorophore;
+
+/**
+ *
+ * @author stefko
+ */
+public class PalmFluorophore extends Fluorophore {
+    private final PalmProperties f_props;
+    private PalmState state;
+    
+    
+    public PalmFluorophore(PalmProperties f_props, Camera camera, double x, double y) {
+        super(camera, x, y);
+        this.f_props = f_props;
+        this.state = PalmState.INACTIVE;
+    }
+
+    @Override
+    public boolean isOn() {
+        return this.state.equals(PalmState.ACTIVE);
+    }
+
+    @Override
+    public boolean isBleached() {
+        return this.state.equals(PalmState.BLEACHED);
+    }
+
+    @Override
+    protected double simulateBrightness() {
+        if (isBleached())
+            return 0.0;
+        double t_rem = 1.0; // remaining time
+        double on_time = 0.0;
+        while (t_rem > 0.0) {
+            switch (state) {
+                case INACTIVE:
+                    double t_activation = nextExponential(f_props.T_a);
+                    if (t_activation < t_rem) {
+                        t_rem -= t_activation;
+                        state = PalmState.ACTIVE;
+                    } else {
+                        t_rem = 0.0;
+                    }
+                    break;
+                    
+                case ACTIVE:
+                    double t_bleach = nextExponential(f_props.T_b);
+                    double t_dark1 = nextExponential(f_props.T_d1);
+                    double t_dark2 = nextExponential(f_props.T_d2);
+                    if (t_bleach < t_rem && t_bleach < t_dark1 && t_bleach < t_dark2) {
+                        on_time += t_bleach;
+                        // we are bleaching
+                        t_rem = 0.0;
+                        state = PalmState.BLEACHED;
+                    } else if (t_dark1 < t_rem && t_dark1 < t_dark2) {
+                        on_time += t_dark1;
+                        t_rem -= t_dark1;
+                        state = PalmState.DARK_1;
+                    } else if (t_dark2 < t_rem) {
+                        on_time += t_dark2;
+                        t_rem -= t_dark2;
+                        state = PalmState.DARK_2;
+                    } else {
+                        t_rem = 0.0;
+                    }
+                    break;
+                    
+                case DARK_1:
+                    double t_recovery_1 = nextExponential(f_props.T_r1);
+                    if (t_recovery_1 < t_rem) {
+                        t_rem -= t_recovery_1;
+                        state = PalmState.ACTIVE;
+                    } else {
+                        t_rem = 0.0;
+                    }
+                    break;
+                    
+                case DARK_2:
+                    double t_recovery_2 = nextExponential(f_props.T_r2);
+                    if (t_recovery_2 < t_rem) {
+                        t_rem -= t_recovery_2;
+                        state = PalmState.ACTIVE;
+                    } else {
+                        t_rem = 0.0;
+                    }
+                    break;
+                    
+                case BLEACHED:
+                    t_rem = 0.0;
+                    break;
+            }
+        }
+        return flicker(on_time*f_props.signal);
+    }
+}
+
+enum PalmState {
+    INACTIVE,
+    ACTIVE,
+    DARK_1,
+    DARK_2,
+    BLEACHED;
+}
---
+++ src/algorithm_tester/generators/realtime/fluorophores/PalmProperties.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2017 stefko
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package algorithm_tester.generators.realtime.fluorophores;
+
+/**
+ *
+ * @author stefko
+ */
+public class PalmProperties {
+    public final double signal;
+    public final double background;
+    
+    public final double k_a;
+    public final double k_b;
+    public final double k_d1;
+    public final double k_d2;
+    public final double k_r1;
+    public final double k_r2;
+    
+    public final double T_a;
+    public final double T_b;
+    public final double T_d1;
+    public final double T_d2;
+    public final double T_r1;
+    public final double T_r2;
+   
+    
+    public PalmProperties(double signal, double background, 
+            double k_a, double k_b, double k_d1, 
+            double k_d2, double k_r1, double k_r2) {
+        if (signal < 0.0 || background < 0.0 || k_a<=0.0 || k_b<0.0 || k_d1<0.0 || k_d2<0.0 || k_r1<0.0 || k_r2<0.0) {
+            throw new IllegalArgumentException();
+        }
+        
+        this.signal = signal;
+        this.background = background;
+        
+        this.k_a = k_a;
+        this.k_b = k_b;
+        this.k_d1 = k_d1;
+        this.k_d2 = k_d2;
+        this.k_r1 = k_r1;
+        this.k_r2 = k_r2;
+        
+        this.T_a = parseInverse(k_a);
+        this.T_b = parseInverse(k_b);
+        this.T_d1 = parseInverse(k_d1);
+        this.T_d2 = parseInverse(k_d2);
+        this.T_r1 = parseInverse(k_r1);
+        this.T_r2 = parseInverse(k_r2);
+    }
+    
+    private static double parseInverse(double d) {
+        if (d<0.0)
+            throw new IllegalArgumentException("Can't parse a negative double.");
+        if (Double.isInfinite(d))
+            return 0.0;
+        else if (d == 0.0)
+            return Double.POSITIVE_INFINITY;
+        else
+            return 1.0/d;
+    }
+}

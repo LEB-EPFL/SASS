0707f3aa67089b41abc176f281c3161c0a8646bc
Marcel Stefko <marcel.stefko@epfl.ch>
2017-05-29 16:16:27 +0200

    Update license header

--- src/simulator/generators/realtime/Runner.java
+++ src/simulator/generators/realtime/Runner.java
@@ -1,9 +1,9 @@
-/*
+/* 
  * Copyright (C) 2017 Laboratory of Experimental Biophysics
  * Ecole Polytechnique Federale de Lausanne
- *
+ * 
  * Author: Marcel Stefko
- *
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or

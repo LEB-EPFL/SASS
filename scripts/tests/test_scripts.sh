4cd551102f2c0ef3236fbc6215ef2d331c62dad8
kmdouglass <kyle.m.douglass@gmail.com>
2017-10-19 12:47:02 +0200

    Updated example scripts (except for 3D and moving fluorophores) to use new API.

--- scripts/tests/test_scripts.sh
+++ scripts/tests/test_scripts.sh
@@ -11,8 +11,7 @@
 # Inputs
 # ------
 # 1. The directory containing the SASS scripts
-# 2. The version of SASS to use (for example, enter 0.2.1 at the
-#    command line). The appropriate .jar file will be downloaded.
+# 2. The SASS .jar file.
 #
 # Kyle M. Douglass, 2017
 #
@@ -28,6 +27,9 @@ java -version
 
 # Download desired version of SASS to a temporary directory
 temp_dir=$(mktemp -d)
+
+# Copy the test data to the temp directory
+cp $test_dir/label_pix_sass.csv $temp_dir
 
 # Change to the temporary directory to run tests there
 cd $temp_dir

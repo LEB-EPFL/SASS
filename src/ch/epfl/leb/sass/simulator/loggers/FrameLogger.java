36c66b665b156d59bd1b48dc64ec0aab58dc2f0f
kmdouglass <kyle.m.douglass@gmail.com>
2018-01-08 07:05:22 +0100

    Added a convenience method to return data from FrameLogger.

--- src/ch/epfl/leb/sass/simulator/loggers/FrameLogger.java
+++ src/ch/epfl/leb/sass/simulator/loggers/FrameLogger.java
@@ -40,8 +40,12 @@ public class FrameLogger extends AbstractLogger{
     private ArrayList<Double> y = new ArrayList();
     private ArrayList<Double> z = new ArrayList();
     private ArrayList<Double> brightness = new ArrayList();
-    private ArrayList<Double> time_on = new ArrayList();
-
+    private ArrayList<Double> timeOn = new ArrayList();
+    
+    /**
+     * Should only the current frame be logged, or all frames?
+     */
+    private boolean logCurrentFrameOnly = false;
 
     /**
      * Constructor is private because FrameLogger is a singleton.
@@ -57,7 +61,12 @@ public class FrameLogger extends AbstractLogger{
     }
 
     /**
-     * Simple logger for positions  and their times.
+     * Logs emitter information for each full frame.
+     * 
+     * Correct operation of this method when logCurrentFrameOnly is true assumes
+     * that values for the frame argument either are the same as previous calls
+     * to this method or monotonically increasing.
+     * 
      * @param frame The current frame
      * @param id The emitter's unique ID.
      * @param x x-position of the emitter
@@ -65,21 +74,68 @@ public class FrameLogger extends AbstractLogger{
      * @param z z-position of the emitter
      * @param brightness the apparent brightness of the fluorophore on the frame
      * in number of photons
-     * @param time_on the amount of time the emitter "id" stays on in the current frame
+     * @param timeOn the amount of time the emitter "id" stays on in the current frame
      */
-    public void logFrame(int frame, int id, double x, double y, double z, double brightness, double time_on) {
+    public void logFrame(
+            int frame,
+            int id,
+            double x,
+            double y,
+            double z,
+            double brightness,
+            double timeOn) {
         if ( !(this.performLogging) )
             return;
 
+        // Save only the current frame's information
+        if (    (this.logCurrentFrameOnly) &&
+                (this.frame.size() > 0) &&
+                (frame != this.frame.get(this.frame.size() - 1))) {
+            this.frame.clear();
+            this.id.clear();
+            this.x.clear();
+            this.y.clear();
+            this.z.clear();
+            this.brightness.clear();
+            this.timeOn.clear();
+        }
+        
         this.frame.add(frame);
         this.id.add(id);
         this.x.add(x);
         this.y.add(y);
         this.z.add(z);
         this.brightness.add(brightness);
-        this.time_on.add(time_on);
+        this.timeOn.add(timeOn);
     }
 
+    /**
+     * Returns all the logged arrays in a single data structure.
+     * 
+     * This method is provided for convenience when all frame information is
+     * required.
+     * 
+     * @return A FramInfo data structure containing all the logged data. 
+     */
+    public ArrayList<FrameInfo> getFrameInfo() {
+        ArrayList<FrameInfo> info = new ArrayList();
+        
+        for (int i = 0; i < this.frame.size(); i++) {
+            info.add(new FrameInfo(
+                    this.frame.get(i),
+                    this.id.get(i),
+                    this.x.get(i),
+                    this.y.get(i),
+                    this.z.get(i),
+                    this.brightness.get(i),
+                    this.timeOn.get(i)
+                )
+            );          
+        }
+        
+        return info;
+    }
+    
     public ArrayList<Integer> getFrame() { return this.frame; }
 
     public ArrayList<Integer> getId() {
@@ -100,7 +156,7 @@ public class FrameLogger extends AbstractLogger{
 
     public ArrayList<Double> getBrightness() { return this.brightness; }
     
-    public ArrayList<Double> getTimeOn() { return this.time_on; }
+    public ArrayList<Double> getTimeOn() { return this.timeOn; }
 
     /**
      * Saves the state of the logger to a file.
@@ -118,15 +174,14 @@ public class FrameLogger extends AbstractLogger{
         printWriter.println("frame,id,x,y,z,brightness,time_on");
 
         for(int ctr = 0; ctr < this.frame.size(); ctr++) {
-            printWriter.printf(
-                    "%d,%d,%.4f,%.4f,%.4f,%.4f,%.4f%n",
+            printWriter.printf("%d,%d,%.4f,%.4f,%.4f,%.4f,%.4f%n",
                     this.frame.get(ctr),
                     this.id.get(ctr),
                     this.x.get(ctr),
                     this.y.get(ctr),
                     this.z.get(ctr),
                     this.brightness.get(ctr),
-                    this.time_on.get(ctr)
+                    this.timeOn.get(ctr)
             );
         }
 
@@ -146,6 +201,28 @@ public class FrameLogger extends AbstractLogger{
         this.y = new ArrayList();
         this.z = new ArrayList();
         this.brightness = new ArrayList();
-        this.time_on = new ArrayList();
+        this.timeOn = new ArrayList();
+    }
+    
+    /**
+     * Toggles whether only the current frame or all frames should be logged.
+     * 
+     * Setting this to true will erase any information already held by the
+     * FrameLogger.
+     * 
+     * @param logCurrentFrame If true, only information on the current frame is
+     * retained.
+     */
+    public void setLogCurrentFrameOnly(boolean logCurrentFrame) {
+        this.logCurrentFrameOnly = logCurrentFrame;
+    }
+    
+    /**
+     * Indicates whether only the current frame or all frames are logged.
+     * @return If return value is true, only information about the current frame
+     *         is returned.
+     */
+    public boolean getLogCurrentFrameOnly() {
+        return this.logCurrentFrameOnly;
     }
 }

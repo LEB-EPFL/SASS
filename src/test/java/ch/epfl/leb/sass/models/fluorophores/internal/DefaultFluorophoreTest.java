5d39b661e1882d5a0b4306a037be88603829d520
kmdouglass <kyle.m.douglass@gmail.com>
2018-05-21 12:53:15 +0200

    Added additional unit tests to DefaultFluorophore's Observable implementation.

--- src/test/java/ch/epfl/leb/sass/models/fluorophores/internal/DefaultFluorophoreTest.java
+++ src/test/java/ch/epfl/leb/sass/models/fluorophores/internal/DefaultFluorophoreTest.java
@@ -60,6 +60,50 @@ public class DefaultFluorophoreTest {
         msg = new FluorophoreStateTransition(42, 42.42, 42, 43);
     }
     
+     /**
+     * Test of addListener and deleteListener methods,
+     * of class DefaultFluorophoreTest.
+     */
+    @Test
+    public void testAddDeleteListeners() {
+        System.out.println("testAddDeleteListeners");
+        TestListener testListener = new TestListener();
+        testListener.isNull = false;
+        TestListener testListener2 = new TestListener();
+        testListener2.isNull = false;
+        
+        double x = 1.5; double y = -2.5; double z = 3.0;
+        int startState = 0;
+        double signal = 1000;
+        when(dummyPSFBuilder.build()).thenReturn(dummyPSF);
+        when(dummyPSFBuilder.eX(x)).thenReturn(dummyPSFBuilder);
+        when(dummyPSFBuilder.eY(y)).thenReturn(dummyPSFBuilder);
+        when(dummyPSFBuilder.eZ(z)).thenReturn(dummyPSFBuilder);
+        when(dummyPSF.getRadius()).thenReturn(1.0);
+        when(dummyStateSystem.getNStates()).thenReturn(startState + 1);
+        Mockito.doNothing()
+               .when(dummyPSF)
+               .generateSignature(ArgumentMatchers.any(ArrayList.class));
+        
+        DefaultFluorophore testFluor = new DefaultFluorophore(
+            dummyPSFBuilder, signal, dummyStateSystem, startState, x, y, z
+        );
+        
+        // Add two listeners, but immediately remove the first.
+        testFluor.addListener(testListener);
+        testFluor.addListener(testListener2);
+        testFluor.deleteListener(testListener);
+        testFluor.setChanged();
+        testFluor.notifyListeners();
+        
+        // Since the listener was removed, isNull should still be false.
+        assert(!testListener.isNull);
+        
+        // testListener2 was not removed, so isNull should now be true.
+        assert(testListener2.isNull);
+
+    }
+    
     /**
      * Test of notifyListeners method, of class DefaultFluorophoreTest.
      */

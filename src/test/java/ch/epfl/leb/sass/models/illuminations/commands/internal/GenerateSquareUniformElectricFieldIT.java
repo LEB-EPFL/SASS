6d55125b4aa39f27f11517fc6423713a0ff94f5e
kmdouglass <kyle.m.douglass@gmail.com>
2018-05-28 17:05:02 +0200

    Add a class to model a uniform sample refractive index.

--- src/test/java/ch/epfl/leb/sass/models/illuminations/commands/internal/GenerateSquareUniformElectricFieldIT.java
+++ src/test/java/ch/epfl/leb/sass/models/illuminations/commands/internal/GenerateSquareUniformElectricFieldIT.java
@@ -18,8 +18,10 @@
 package ch.epfl.leb.sass.models.illuminations.commands.internal;
 
 import ch.epfl.leb.sass.models.samples.RefractiveIndex;
+import ch.epfl.leb.sass.models.samples.internal.UniformRefractiveIndex;
 import ch.epfl.leb.sass.models.illuminations.ElectricField;
 import ch.epfl.leb.sass.models.illuminations.commands.ElectricFieldCommand;
+
 import org.apache.commons.math.util.MathUtils;
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.complex.ComplexUtils;
@@ -40,17 +42,16 @@ import static org.mockito.Mockito.*;
 public class GenerateSquareUniformElectricFieldIT {
     
     private double wavelength = 0.532;
-    private RefractiveIndex dummyRefractiveIndex;
+    private RefractiveIndex refractiveIndex = new UniformRefractiveIndex(new Complex(1.0));
     private GenerateSquareUniformElectricField.Builder builder;
     private Vector3D orientation = new Vector3D(1.0, 0.0, 0.0);
     
     @Before
     public void setUp() {
-        dummyRefractiveIndex = mock(RefractiveIndex.class);
         builder = new GenerateSquareUniformElectricField.Builder();
         
         builder.width(20).height(30).orientation(Vector3D.PLUS_I)
-               .refractiveIndex(dummyRefractiveIndex).wavelength(0.532);
+               .refractiveIndex(refractiveIndex).wavelength(0.532);
     }
 
     /**
@@ -62,7 +63,6 @@ public class GenerateSquareUniformElectricFieldIT {
         ElectricField instance = cmd.generateElectricField();
         
         // Inside the illumination area
-        when(dummyRefractiveIndex.getN(10, 20, 0)).thenReturn(new Complex(1.0));
         Complex result = instance.getEx(10, 20, 0);
         assertEquals(1, result.getReal(), 0.0);
         assertEquals(0, result.getImaginary(), 0.0);
@@ -72,35 +72,26 @@ public class GenerateSquareUniformElectricFieldIT {
                 = ComplexUtils.polar2Complex(orientation.getX(),
                                           MathUtils.TWO_PI / wavelength * 1.0);
         
-        when(dummyRefractiveIndex.getN(10, 20, 1)).thenReturn(new Complex(1.0));
         result = instance.getEx(10, 20, 1);
         assertEquals(expResult.getReal(), result.getReal(), 0.0);
         assertEquals(expResult.getImaginary(), result.getImaginary(), 0.0);
         
         // Outside the illumination area
-        when(dummyRefractiveIndex.getN(-10, 20, 10))
-           .thenReturn(new Complex(1.0));
         result = instance.getEx(-10, 20, 10);
         assertEquals(0, result.getReal(), 0.0);
         assertEquals(0, result.getImaginary(), 0.0);
         
         // Outside the illumination area
-        when(dummyRefractiveIndex.getN(30, 20, -10))
-           .thenReturn(new Complex(1.0));
         result = instance.getEx(30, 20, -10);
         assertEquals(0, result.getReal(), 0.0);
         assertEquals(0, result.getImaginary(), 0.0);
         
         // Outside the illumination area
-        when(dummyRefractiveIndex.getN(10, -10, 100))
-           .thenReturn(new Complex(1.0));
         result = instance.getEx(10, -10, 100);
         assertEquals(0, result.getReal(), 0.0);
         assertEquals(0, result.getImaginary(), 0.0);
         
         // Outside the illumination area
-        when(dummyRefractiveIndex.getN(10, 50, -100))
-           .thenReturn(new Complex(1.0));
         result = instance.getEx(10, 50, -100);
         assertEquals(0, result.getReal(), 0.0);
         assertEquals(0, result.getImaginary(), 0.0);
